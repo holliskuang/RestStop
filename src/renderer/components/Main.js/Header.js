2d49a369c0b7020a47b28e9b15076378b5efb81c
holliskuang <kuanghollis@gmail.com>
2023-01-26 00:21:56 -0800

    Fixed Bug to correctly delete

--- src/renderer/components/Main.js/Header.js
+++ src/renderer/components/Main.js/Header.js
@@ -2,27 +2,26 @@ import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
-export default function Header() {
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+export default function Header(props) {
   const [key, setKey] = useState('');
   const [value, setValue] = useState('');
   const [checked, setChecked] = useState(false);
 
-
-  // if checked==true , retrieve key value 
+  // if checked==true , retrieve key value
 
   const handleClick = () => {
     setChecked(event.target.checked);
     // add to array in redux
-  }
+  };
   return (
     <Box>
-      <Checkbox
-        checked={checked}
-        onChange={handleClick}
-      ></Checkbox>
+      <Checkbox checked={checked} onChange={handleClick}></Checkbox>
       <TextField
         variant="standard"
         label="Key"
+        value={key}
         onChange={(event) => {
           setKey(event.target.value);
         }}
@@ -30,10 +29,18 @@ export default function Header() {
       <TextField
         variant="standard"
         label="Value"
+        value={value}
         onChange={(event) => {
           setValue(event.target.value);
         }}
       ></TextField>
+      <IconButton
+        onClick={() => {
+          props.delete(props.id);
+        }}
+      >
+        <DeleteIcon />
+      </IconButton>
     </Box>
   );
 }
--- src/renderer/components/Main.js/HeaderBox.js
+++ src/renderer/components/Main.js/HeaderBox.js
@@ -3,14 +3,37 @@ import { IconButton } from '@mui/material';
 import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import Header from './Header';
-export default function HeaderBox() {
-  const [headerList, setHeaderList] = useState([uuid()]);
+import Box from '@mui/material/Box/Box';
 
-  const addHeader = () => {};
+export default function HeaderBox() {
+  const initalObj = {};
+  initalObj[uuid()] = { key: '', value: '' };
+  const [headerList, setHeaderList] = useState(initalObj);
+
+  /*const onChangeKey = (id, event) => {
+    console.log(headerList)
+    headerList[id].key = event.currentTarget.value;
+    setHeaderList({ ...headerList });
+  } */
+  const addHeader = () => {
+    const newHeader = {};
+    newHeader[uuid()] = { key: '', value: '' };
+    setHeaderList({ ...headerList, ...newHeader });
+  };
+  const deleteFromHeaderList = (id) => {
+    console.log(id);
+    if (Object.keys(headerList).length === 1) return;
+    delete headerList[id];
+    setHeaderList({ ...headerList});
+  };
   return (
-    <IconButton onClick={addHeader}>
-      <AddBoxOutlinedIcon />
-    </IconButton>
-    HeaderList.map((header) => <Header id={headerId} />)
+    <Box>
+      <IconButton onClick={addHeader}>
+        <AddBoxOutlinedIcon />
+      </IconButton>
+      {Object.keys(headerList).map((id) => {
+        return <Header id={id} key={id} delete={deleteFromHeaderList} onChangeKey={onChangeKey}/>;
+      })}
+    </Box>
   );
 }
