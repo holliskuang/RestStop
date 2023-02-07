ce20972f6e0b3a2506a7a1a40532905dac3f4a88
holliskuang <kuanghollis@gmail.com>
2023-02-06 20:37:37 -0800

    WS On Handling

--- src/main/WebsocketController.ts
+++ src/main/WebsocketController.ts
@@ -1,18 +1,38 @@
+import { ipcMain } from 'electron';
 import ws from 'ws';
 
-
-const WebSocket = typeof window === 'undefined' ? ws : window.WebSocket;
-
 export default function WebSocketController(reqResObj, mainWindow): void {
+  const ws = new ws(reqResObj.url);
 
-    const ws = new WebSocket(reqResObj.url, reqResObj.headers);
-    
-    ws.on('open', function open() {
-        ws.send('something');
-    });
-    
-    ws.on('message', function incoming(data) {
-        console.log(data);
-    });
+  ws.on('open', function open() {
+    ws.send('Hello!');
+  });
 
+  // Transfer Message From Renderer to Main and send it through the websocket
+  ipcMain.on('clientMessage', (event, arg) => {
+    ws.send(arg);
+  });
+
+  // Transfer Message From Main to Renderer that is received from the websocket
+
+  ws.on('message', function incoming(data) {
+    mainWindow.webContents.send('serverMessage', data);
+  });
+
+  // handle websocket errors and unexpected responses
+  ws.on('error', function error(err) {
+    mainWindow.webContents.send('serverMessage', err.message);
+  });
+  ws.on('unexpected-response', function unexpectedResponse(req, res) {
+    mainWindow.webContents.send('serverMessage', res.statusCode);
+  });
+
+  // handle websocket close
+  ws.on('close', function close() {
+    mainWindow.webContents.send('serverMessage', 'WebSocket closed');
+  });
+  // initialize websocket close
+  ipcMain.on('closeWebSocket', (event, arg) => {
+    ws.close();
+  });
 }
