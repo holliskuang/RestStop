f33bfb2d9c6bbfb1fcd784ce22e9404f22d57294
holliskuang <kuanghollis@gmail.com>
2023-02-06 20:25:30 -0800

    ws initial messages to know if handshake suceeded

--- src/main/WebsocketController.ts
+++ src/main/WebsocketController.ts
@@ -1,2 +1,18 @@
 import ws from 'ws';
-import NodeWebSocket from 'ws';
+
+
+const WebSocket = typeof window === 'undefined' ? ws : window.WebSocket;
+
+export default function WebSocketController(reqResObj, mainWindow): void {
+
+    const ws = new WebSocket(reqResObj.url, reqResObj.headers);
+    
+    ws.on('open', function open() {
+        ws.send('something');
+    });
+    
+    ws.on('message', function incoming(data) {
+        console.log(data);
+    });
+
+}
