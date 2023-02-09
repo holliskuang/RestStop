081c0db61ed6c548dacd9225e522fcda2c867357
holliskuang <kuanghollis@gmail.com>
2023-02-09 00:36:58 -0800

    DateStamps added

--- src/main/WebsocketController.ts
+++ src/main/WebsocketController.ts
@@ -12,34 +12,34 @@ export const WebSocketController = {
     reqResObj['response'] = { serverMessage: [], clientMessage: [] };
     ws.on('open', function open() {
       reqResObj.connectionStatus = 'open';
-      event.sender.send('serverMessage', [reqResObj, Date.now()]);
+      event.sender.send('serverMessage', reqResObj);
     });
 
     // handle websocket errors and unexpected responses
     ws.on('error', function error(err) {
-      reqResObj.response.serverMessage.push(err.message);
-      event.sender.send('serverMessage', [reqResObj, Date.now()]);
+      reqResObj.response.serverMessage.push([err.message, Date.now()]);
+      event.sender.send('serverMessage', reqResObj);
     });
     ws.on('unexpected-response', function unexpectedResponse(req, res) {
-      reqResObj.response.serverMessage.push(res.statusCode);
-      event.sender.send('serverMessage', [reqResObj, Date.now()]);
+      reqResObj.response.serverMessage.push([res.statusCode, Date.now()]);
+      event.sender.send('serverMessage', reqResObj);
     });
 
     // handle websocket close
     ws.on('close', function close() {
-      reqResObj.response.serverMessage.push('WebSocket closed');
+      reqResObj.response.serverMessage.push(['WebSocket closed', Date.now()]);
       reqResObj.connectionStatus = 'closed';
-      event.sender.send('serverMessage', [reqResObj, Date.now()]);
+      event.sender.send('serverMessage', reqResObj);
     });
   },
-
+  
   closeWebSocket: (event, reqResObj) => {
     this.websocket.close();
     reqResObj.connectionStatus = 'closed';
-    event.sender.send('serverMessage', [reqResObj, Date.now()]);
+    event.sender.send('serverMessage', reqResObj);
   },
   TransferMessageToWebSocket: (event, message, reqResObj) => {
-    const ws = this.websocket;
+    const ws=this.websocket;
     if (ws == null) {
       return;
     }
@@ -52,11 +52,13 @@ export const WebSocketController = {
       event.sender.send('serverMessage', reqResObj);
     });
 
-    reqResObj.response.clientMessage.push(message);
+    reqResObj.response.clientMessage.push([message, Date.now()]);
     console.log('pushing message', message);
     console.log('reqResObj', reqResObj);
     event.sender.send('serverMessage', [reqResObj, Date.now()]);
     this.websocket.send(message);
+
+    
   },
 };
 ///// Diff OBJS updating to state, only one survives
