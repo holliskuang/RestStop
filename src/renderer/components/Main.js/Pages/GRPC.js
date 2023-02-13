80ffcad5e054826f23bb4144fcf337cc0726c16b
holliskuang <kuanghollis@gmail.com>
2023-02-13 01:17:18 -0800

    GRPC Page Layout

--- src/renderer/components/Main.js/Pages/GRPC.js
+++ src/renderer/components/Main.js/Pages/GRPC.js
@@ -1,7 +1,20 @@
 import { ipcRenderer } from 'electron';
 import FileUploadSingle from '../Widgets/Upload';
+import React, { useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { createTheme } from '@mui/material/styles';
+import { useDispatch } from 'react-redux';
+import { setMethod, setResponseMode } from '../../../state/currentReqRes.js';
 
 export default function GRPC() {
+  const mode = useSelector((state) => state.light.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const dispatch = useDispatch();
+  const response = useSelector((state) => state.currentReqRes.response);
+  useEffect(() => {
+    dispatch(setMethod('GRPC'));
+    dispatch(setResponseMode('GRPC'));
+  }, []);
   const api = window.api.ipcRenderer;
 
   api.receive('grpcData', (event, data) => {
@@ -11,8 +24,14 @@ export default function GRPC() {
     console.log(data);
   });
   return (
-    <div>
-      <FileUploadSingle />
-    </div>
+    <Box>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar></NavBar>
+        <WSRequest />
+        <FileUploadSingle />
+        <Dashboard />
+      </ThemeProvider>
+    </Box>
   );
 }
